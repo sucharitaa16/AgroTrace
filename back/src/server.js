181a55e5f4093964ba9ac19d17425943d30cb47a
samrat-ghosh-007 <samrat9ghosh2018@gmail.com>
2026-05-25 19:35:05 +0530

    Fixed Inconsistencies

--- back/src/server.js
+++ back/src/server.js
@@ -6,7 +6,7 @@ const connectDB = require("./config/db");
 
 connectDB();
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(
     `Server running on port ${process.env.PORT}`
   );
